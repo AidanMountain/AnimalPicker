816f84f70839ae2cab6e4598918f768b6bc97aca
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-28 16:33:21 -0500

    created a scanner and if statements for user input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to see a dog or a cat?");
+        String animal = scanner.nextLine();
+
+        if(animal == "cat"){
+            System.out.println("INSTER CAT ASCII HERE");
+        }
+        else if(animal == "dog"){
+            System.out.println("INSTER DOG ASCII HERE");
+        }
     }
+
 }
