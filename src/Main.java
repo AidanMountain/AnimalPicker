ef22d5db7653a72acd9b2b58663f6a9f5723244b
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-28 17:03:22 -0500

    made program continually ask if the input is not valid

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        boolean isValid = true;
+
         //System.out.println("Hello world!");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Would you like to see a dog or a cat?");
         String animal = scanner.nextLine();
         System.out.println(animal);
-        if(animal.equals("cat")){
-            System.out.println("INSTERT CAT ASCII HERE");
+        while(isValid){
+            if(animal.equals("cat")){
+                System.out.println("INSTERT CAT ASCII HERE");
+            }
+            else if(animal.equals("dog")){
+                System.out.println("INSTERT DOG ASCII HERE");
+            }
+            else{
+                isValid = false;
+                System.out.println("ERROR: INVALID INPUT");
+            }
         }
-        else if(animal.equals("dog")){
-            System.out.println("INSTERT DOG ASCII HERE");
-        }
-        else System.out.println("ERROR: INVALID INPUT");
+
     }
 
 }
